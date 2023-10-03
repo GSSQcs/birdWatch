6ca4e4178fdc3f3b72a060f693a5a80d41535496
sharmarkeGit <shar.alternatif@gmail.com>
2023-10-03 16:27:08 -0400

    creating home compo with searchbar" q

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,20 @@
 import React from "react";
+import '../stylesheets/Home.css'
+import SearchBar from "./SearchBar";
 
 
 
 const Home = () => {
 
     return (
-        <></>
+        <>
+        <nav className="navbar"><h2>Bird Watch</h2></nav>
+        <div className="search-bar"><input type="text" placeholder="location"/>
+        <SearchBar/></div>
+        <div className="content">
+            <div></div>
+        </div>
+        </>
     )
 
 }
