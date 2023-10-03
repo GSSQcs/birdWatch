f27e7acd6f71f69c6d71316a6f0579a3d07d341b
sharmarkeGit <shar.alternatif@gmail.com>
2023-10-03 16:25:03 -0400

    added Home compo

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import BirdsCollectionComponent from './components/BirdsCollectionComponent';
+import Home from './components/Home';
 
 function App() {
   
   return (
 
     <div>
-      <h1>coucouc</h1>
+      <Home></Home>
       <BirdsCollectionComponent />
   
     </div>
