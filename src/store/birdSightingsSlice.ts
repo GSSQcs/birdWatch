14952e4aee9ae6f15651f389613e76e9728812fe
sharmarkeGit <shar.alternatif@gmail.com>
2023-10-04 09:17:23 -0400

    new component to sotre the api results

---
+++ src/store/birdSightingsSlice.ts
@@ -0,0 +1,22 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface BirdSightingsState {
+    birdSightings: object[];
+}
+
+const initialState: BirdSightingsState = {
+    birdSightings: []
+}
+
+const birdSightingsSlice = createSlice({
+    name: 'birdSightings',
+    initialState,
+    reducers: {
+        setBirdSightings: (state, action: PayloadAction<object[]>) => {
+            state.birdSightings = action.payload;
+        }
+    }
+});
+
+export const {setBirdSightings} = birdSightingsSlice.actions
+export default birdSightingsSlice.reducer
