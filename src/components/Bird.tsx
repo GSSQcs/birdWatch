7cd2378dddf511bb75cea756b54d62b70a34601e
sharmarkeGit <shar.alternatif@gmail.com>
2023-10-04 09:12:09 -0400

    adapted bird component for grid

--- src/components/Bird.tsx
+++ src/components/Bird.tsx
@@ -6,28 +6,44 @@ export interface BirdProps {
 
 export function Bird({ comName,  obsDt, locName }: BirdProps) {
     return (
-        <div className="bird">
-            <span>
-                {comName}
-            </span>
-            <span>
-                {obsDt}
-            </span>
-            <span>
-                {locName}
-            </span>
-            <span>
+        <>
+        {/* // <div className="bird grid-container"> */}
+            <div>
+                <span>
+                    {comName}
+                </span>
+            </div>
+            <div>
+                <span>
+                    {obsDt}
+                </span>
+            </div>
+            <div>
+                <span>
+                    {locName}
+                </span>
+            </div>
+            
+            <div className="span-three">
+            {/* <div>    */}
+                <span>
+                    <input type='checkbox'
+                        id={comName + '-wishlist'}
+                        name={comName + '-wishlist'} />
+                    <label htmlFor={comName + '-wishlist'}> Add to wishlist?</label>
+                </span>
+            {/* </div> */}
+            {/* <div> */}
+                <span>
                 <input type='checkbox'
-                    id={comName + '-wishlist'}
-                    name={comName + '-wishlist'} />
-                <label htmlFor={comName + '-wishlist'}> Add to wishlist?</label>
-            </span>
-            <span>
-            <input type='checkbox'
-                    id={comName + '-seen'}
-                    name={comName + '-seen'} />
-                <label htmlFor={comName + '-seen'}> Add to wishlist?</label>
-            </span>
-        </div>
+                        id={comName + '-seen'}
+                        name={comName + '-seen'} />
+                    <label htmlFor={comName + '-seen'}> Seen?</label>
+                </span>
+            {/* </div>  */}
+     </div>
+        </>
+     
+        
     )
 }
